4c6d929ceb3c1eb57ba7acf516194cf985e78e4e
dumin199101 <1766266374@qq.com>
2020-05-28 19:19:06 +0800

    IDEA 包使用

--- 01Grammar/src/com/goosuu/code01/ConstVariable.java
+++ 01Grammar/src/com/goosuu/code01/ConstVariable.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+package com.goosuu.code01;
 
 /**
  * 常量：
--- 01Grammar/src/com/goosuu/code01/HelloWorld.java
+++ 01Grammar/src/com/goosuu/code01/HelloWorld.java
@@ -1,5 +1,7 @@
+package com.goosuu.code01;
+
 public class HelloWorld {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         /*输出HelloWorld*/
         System.out.println("HelloWorld");
     }
