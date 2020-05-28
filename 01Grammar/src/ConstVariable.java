f35259c911c447a1e0978234dbf37d39211ecd4e
dumin199101 <1766266374@qq.com>
2020-05-28 19:06:12 +0800

    Java 常量

---
+++ 01Grammar/src/ConstVariable.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+/**
+ * 常量：
+ *   1.字符串常量 "ABC"
+ *   2.整数常量 1
+ *   3.浮点数常量 1.2
+ *   4.字符常量  'A','中'  -- 两个单引号之间有且只有一个字符，''这种写法是错误的
+ *   5.布尔常量 true,false
+ *   6.空常量 null  -- 不能直接打印
+ */
+public class ConstVariable {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println(1);
+        System.out.println(3.14);
+        System.out.println('A');
+        System.out.println('中');
+        System.out.println(true);
+    }
+}
---
+++ 01Grammar/src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+     public static void main(String[] args) {
+        /*输出HelloWorld*/
+        System.out.println("HelloWorld");
+    }
+}
