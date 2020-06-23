20eb5867715c6586969ba55731d5a37e91c84c13
dumin199101 <1766266374@qq.com>
2020-06-23 10:47:17 +0800

    JaveEE之Maven项目构建

---
+++ 02JavaWeb/src/com/goosuu/code08/MavenDemo.java
@@ -0,0 +1,43 @@
+package com.goosuu.code08;
+
+/**
+ * Maven基础:
+ *   定义：Maven是一个项目管理工具。
+ *   Maven一键构建项目：
+ *      清理--编译--测试--运行--打包--安装--部署
+ *      项目构建时，后边的进程会重复前边的进程
+ *   Maven常用命令：
+ *     clean
+ *     compile
+ *     test
+ *     package
+ *     install
+ *   Maven项目结构：
+ *      src:
+ *        main:
+ *          java
+ *          resources
+ *          webapp
+ *        test:
+ *          java
+ *          resources
+ *          webapp
+ *        target
+ *        pom.xml
+ *  Maven仓库：
+ *    本地仓库
+ *    远程仓库（私服）
+ *    中央仓库：https://mvnrepository.com/
+ *  Maven依赖范围：
+ *    compile:编译、测试、运行
+ *    provided:编译、测试
+ *    test:测试
+ *    runtime:测试、运行
+ *
+ *
+ *
+ *
+ *
+ */
+public class MavenDemo {
+}
